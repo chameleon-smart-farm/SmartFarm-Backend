b9f50d1ea39af020acfd8082a4fdb155dcfc7cf7
BakHyegyeong <calla3494qhk@naver.com>
2025-03-27 22:40:51 +0900

    feat : 사용자 이름 API 추가

--- src/main/java/com/smartfarm/chameleon/domain/login/dao/LoginMapper.java
+++ src/main/java/com/smartfarm/chameleon/domain/login/dao/LoginMapper.java
@@ -6,6 +6,10 @@ import com.smartfarm.chameleon.domain.login.dto.UserDTO;
 @Mapper
 public interface LoginMapper {
     
+    // 사용자 로그인
     public UserDTO login (UserDTO userDTO);
+
+    // 사용자 이름 반환
+    public String read_user_name (String user_id);
 
 }
