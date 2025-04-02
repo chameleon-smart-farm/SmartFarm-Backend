6ca1ebd344090e0ff7798422bbb987de62e0224c
BakHyegyeong <calla3494qhk@naver.com>
2025-04-03 00:07:12 +0900

    fix :  회원가입 로직 수정

--- src/main/java/com/smartfarm/chameleon/domain/user/dao/UserMapper.java
+++ src/main/java/com/smartfarm/chameleon/domain/user/dao/UserMapper.java
@@ -3,6 +3,7 @@ package com.smartfarm.chameleon.domain.user.dao;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.smartfarm.chameleon.domain.login.dto.UserDTO;
+import com.smartfarm.chameleon.domain.user.dto.SignUpDTO;
 
 @Mapper
 public interface UserMapper {
@@ -15,5 +16,8 @@ public interface UserMapper {
 
     // 시리얼 번호 확인 후 house_id 반환
     public int validate_serial(String serial);
+
+    // 사용자 회원가입 : 사용자 이름, 아이디, 비밀번호, 관심 작물 입력
+    public void sign_up(SignUpDTO signUpDTO);
     
 }
