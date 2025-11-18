d9421297e68ccbfbd911335e95a8b04c934e81a2
BakHyegyeong <calla3494qhk@naver.com>
2025-11-18 09:57:04 +0900

    fix : CloudFront 주소로 변경

--- src/main/java/com/smartfarm/chameleon/global/config/WebConfig.java
+++ src/main/java/com/smartfarm/chameleon/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8077", "http://52.79.219.210:8077")   // local, 배포 환경의 프론트 엔드 주소
+                .allowedOrigins("http://localhost:8077", "https://moduleup.cloud")   // local, 배포 환경의 프론트 엔드 주소
                 .allowedMethods("*")            // 모든 HTTP Method 허용
                 .allowedHeaders("*")            // 모든 HTTP 헤더 허용
                 .allowCredentials(true);  // 쿠키 인증 요청 허용
